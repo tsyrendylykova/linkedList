a079359ce75c0f744d54e5a559cfac67f061ed9c
Цырендылыкова Эржена <tsyrendylykova@Cyrendylykovas-MacBook-Pro.local>
2019-01-20 19:39:16 +0300

    [add] iadd method

--- linked_list.py
+++ linked_list.py
@@ -6,8 +6,9 @@ class List:
     def print(self):
         cur = self
         while cur:
-            print(cur.value)
+            print(cur.value, end=" ")
             cur = cur.next_
+        print("", flush = True)
 
     def append(self, value):
         cur = self
@@ -15,9 +16,35 @@ class List:
             cur = cur.next_
         cur.next_ = List(value, None)
 
+    def __iadd__(self, other):
+        if other is None:
+            return self
+        cur = self
+        while cur.next_:
+            cur = cur.next_
+        if isinstance(other, List):
+            cur.next_ = List(other.value)
+            cur = cur.next_
+            while other.next_:
+                other = other.next_
+                cur.next_ = List(other.value)
+                cur = cur.next_
+        elif isinstance(other, list):
+            # todo
+            print("todo")
+        return self
+
+
     
 list_ = List(value=1, next_=List(value=2, next_=List(value=3)))
 list_.print()
 
 list_.append(4)
+list_.print()
+
+tail = List(value=5, next_=List(value=6))
+list_ += tail
+if list_ == None:
+    print('im noen')
+
 list_.print()
