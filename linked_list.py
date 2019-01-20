bb4701b56f0b2d1f2d70b7d577c7f1cadff28d7e
Цырендылыкова Эржена <tsyrendylykova@Cyrendylykovas-MacBook-Pro.local>
2019-01-20 16:00:04 +0300

    [add] revrese print

--- linked_list.py
+++ linked_list.py
@@ -10,3 +10,30 @@ class List:
 
     def clear(self):
         self.__init__()
+
+    def push(self, value):
+        if self.head is not None:
+            self.head = Node(value, self.head)
+        else:
+            self.head = Node(value, None)
+            self.tail = self.head
+
+    def print_(self):
+        curr_node = self.head
+        while curr_node:
+            print(curr_node.value)
+            curr_node = curr_node.next_
+
+    def reverse(self):
+        prev_node = None
+        curr_node = self.head
+        while curr_node:
+            next_node = curr_node.next_
+            curr_node.next_ = prev_node
+            prev_node = curr_node
+            curr_node = next_node
+        self.head = prev_node
+        
+    def print_reversed(self):
+        self.reverse()
+        self.print_()
