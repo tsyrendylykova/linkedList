8fe533f993485904e586004c2440b3c30af748a6
Цырендылыкова Эржена <tsyrendylykova@Cyrendylykovas-MacBook-Pro.local>
2019-01-20 22:43:13 +0300

    [add] test_iadd

--- test_linked_list.py
+++ test_linked_list.py
@@ -18,7 +18,20 @@ class TestList(unittest.TestCase):
         self.assertEqual(list_.value, 1)
         self.assertEqual(list_.next_._value, 2)
         self.assertEqual(list_.next_.next_.value, 3) 
-        self.assertEqual(list_.next_.next_.next_.value, 4) 
+        self.assertEqual(list_.next_.next_.next_.value, 4)
+
+    def test_iadd(self):
+        list_ = List(value=1, next_=List(value=2, next_=List(value=3)))
+        tail = List(value=5, next_=List(value=6))
+        list_ += tail
+
+        self.assertEqual(list_.value, 1)
+        self.assertEqual(list_.next_._value, 2)
+        self.assertEqual(list_.next_.next_.value, 3) 
+        self.assertEqual(list_.next_.next_.next_.value, 5)
+        self.assertEqual(list_.next_.next_.next_.next_.value, 6)
+        self.assertEqual(list_.next_.next_.next_.next_.next_, None)
+
 
 if __name__ == '__main__':
     unittest.main()
