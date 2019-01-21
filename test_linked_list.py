1dacdfa4e7cff1a0d871720f04539ece9b71f412
Цырендылыкова Эржена <tsyrendylykova@Cyrendylykovas-MacBook-Pro.local>
2019-01-21 23:37:26 +0300

    [mod] test_iadd

--- test_linked_list.py
+++ test_linked_list.py
@@ -32,6 +32,14 @@ class TestList(unittest.TestCase):
         self.assertEqual(list_.next_.next_.next_.next_.value, 6)
         self.assertEqual(list_.next_.next_.next_.next_.next_, None)
 
+        tail._value = 0
+        self.assertEqual(list_.value, 1)
+        self.assertEqual(list_.next_._value, 2)
+        self.assertEqual(list_.next_.next_.value, 3) 
+        self.assertEqual(list_.next_.next_.next_.value, 5)
+        self.assertEqual(list_.next_.next_.next_.next_.value, 6)
+        self.assertEqual(list_.next_.next_.next_.next_.next_, None)
+
     def test_value(self):
         list_ = List(value=1, next_=List(value=2))
 
